9ebb297a7ef2019ee52d37a6f9d329ac245f7d7c
Flavio <fegemo@gmail.com>
2014-11-14 13:03:37 -0200

    Commit inicial

---
+++ principal.js
@@ -0,0 +1,13 @@
+// Faça seu exercício neste arquivo
+
+
+// Alguns elementos importantes na página (index.html):
+// #rolar: Botão rolar
+//	 - você deve atribuir um _handler_ de evento a ele para fazer o cálculo da rolagem dos dados
+// #resultado: Elemento cujo conteúdo deve ser uma string com os resultados
+//	 - você deve definir seu conteúdo (.innerHTML) com a string de resultados
+// #recipienteResultados: "Container" do elemento dos resultados: 
+//	 - você deve torná-lo visível removendo a classe "oculto" dele
+// .quantidade: todos os input[type=number] com a quantidade de dados a serem rolados
+// #quantidadeD{4,6,8,10,12,20}: um ID para cada input[type=number] com a quantidade
+
