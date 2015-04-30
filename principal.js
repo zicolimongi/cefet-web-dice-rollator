2f2789c81d73006db2e5da0399839e0ade18fbde
Zico Limongi <zicolimongi@gmail.com>
2015-04-30 12:19:28 -0300

    Update principal.js
    commit inicial

--- principal.js
+++ principal.js
@@ -1,13 +1,18 @@
-// Faça seu exercício neste arquivo
+// FaÃ§a seu exercÃ­cio neste arquivo
 
 
-// Alguns elementos importantes na página (index.html):
-// #rolar: Botão rolar
-//	 - você deve atribuir um _handler_ de evento a ele para fazer o cálculo da rolagem dos dados
-// #resultado: Elemento cujo conteúdo deve ser uma string com os resultados
-//	 - você deve definir seu conteúdo (.innerHTML) com a string de resultados
+// Alguns elementos importantes na pÃ¡gina (index.html):
+// #rolar: BotÃ£o rolar
+//	 - vocÃª deve atribuir um _handler_ de evento a ele para fazer o cÃ¡lculo da rolagem dos dados
+// #resultado: Elemento cujo conteÃºdo deve ser uma string com os resultados
+//	 - vocÃª deve definir seu conteÃºdo (.innerHTML) com a string de resultados
 // #recipienteResultados: "Container" do elemento dos resultados: 
-//	 - você deve torná-lo visível removendo a classe "oculto" dele
+//	 - vocÃª deve tornÃ¡-lo visÃ­vel removendo a classe "oculto" dele
 // .quantidade: todos os input[type=number] com a quantidade de dados a serem rolados
 // #quantidadeD{4,6,8,10,12,20}: um ID para cada input[type=number] com a quantidade
 
+var playButton = document.getElementById('rolar'),
+    dicesSum = document.getElementById('resultado'),
+    resultContent = document.getElementById('recipienteResultados');
+    
+    
